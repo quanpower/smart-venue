7a72f7827963b13d4a8764e9834f2776626f0591
quanpower <quanpower@gmail.com>
2017-11-28 17:30:36 +0800

    * 1.separate models

---
+++ app/models/__init__.py
@@ -0,0 +1,13 @@
+from .user import Permission, Role, Follow, User, AnonymousUser, Post, Comment
+
+
+__all__ = [
+    'User',
+    'Permission',
+    'Role',
+    'Follow',
+    'User',
+    'AnonymousUser',
+    'Post',
+    'Comment',
+]
